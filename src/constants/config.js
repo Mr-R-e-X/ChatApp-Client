4433a329a71c5a1b91a4344751d9857a27394efd
MrRex <souvikhazra151@gmail.com>
2024-10-05 11:11:04 +0530

    bug fixed

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-export const server = import.meta.env.VITESERVER;
-export const StunServer1 = import.meta.env.VITEGOOGLESTUNSERVER1;
-export const StunServer2 = import.meta.env.VITEGOOGLESTUNSERVER2;
-export const StunServer3 = import.meta.env.VITETWILOSERVER;
+export const server = import.meta.env.VITE_SERVER;
+export const StunServer1 = import.meta.env.VITE_GOOGLE_STUN_SERVER1;
+export const StunServer2 = import.meta.env.VITE_GOOGLE_STUN_SERVER2;
+export const StunServer3 = import.meta.env.VITE_TWILO_SERVER;
